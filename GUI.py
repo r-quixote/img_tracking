5c67bdced6322b0d22d086eebdb14ccb09af4ea3
r-quixote <45530447+r-quixote@users.noreply.github.com>
2019-09-22 12:23:11 +0300

    added gui ideas?

---
+++ GUI.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 27 15:49:29 2019
+
+@author: YasmineMnb
+"""
+
+
+# import the necessary packages
+from tkinter import *
+
+from PIL import Image
+from PIL import ImageTk
+from tkinter import filedialog
+import cv2
+
+import img_procesing
+
+
+def select_image():
+	# grab a reference to the image panels
+	global panelA, panelB
+
+	# open a file chooser dialog and allow the user to select an input
+	# image
+	path = filedialog.askopenfilename()
+
+    # ensure a file path was selected
+	if len(path) > 0:
+		# load the image from disk, convert it to grayscale, and detect
+		# edges in it
+		image = cv2.imread(path)
+		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		edged = cv2.Canny(gray, 50, 100)
+
+		# OpenCV represents images in BGR order; however PIL represents
+		# images in RGB order, so we need to swap the channels
+		PIL_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+		# convert the images to PIL format...
+		image = Image.fromarray(image)
+		edged = Image.fromarray(edged)
+
+		# ...and then to ImageTk format
+		image = ImageTk.PhotoImage(image)
+		edged = ImageTk.PhotoImage(edged)
+
+		# if the panels are None, initialize them
+		if panelA is None or panelB is None:
+			# the first panel will store our original image
+			panelA = Label(image=image)
+			panelA.image = image
+			panelA.pack(side="left", padx=10, pady=10)
+
+			# while the second panel will store the edge map
+			panelB = Label(image=edged)
+			panelB.image = edged
+			panelB.pack(side="right", padx=10, pady=10)
+
+		# otherwise, update the image panels
+		else:
+			# update the pannels
+			panelA.configure(image=image)
+			panelB.configure(image=edged)
+			panelA.image = image
+			panelB.image = edged
+
+
+# initialize the window toolkit along with the two image panels
+root = Tk()
+panelA = None
+panelB = None
+
+# create a button, then when pressed, will trigger a file chooser
+# dialog and allow the user to select an input image; then add the
+# button the GUI
+btn = Button(root, text="Select an image", command=select_image)
+btn.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+
+# kick off the GUI
+root.mainloop()
+#%%
+
+import tkinter as tk
+
+LARGE_FONT= ("Verdana", 12)
+
+class SeaofBTCapp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand = True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        frame = StartPage(container, self)
+
+        self.frames[StartPage] = frame
+
+        frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+
+app = SeaofBTCapp()
+app.mainloop()
