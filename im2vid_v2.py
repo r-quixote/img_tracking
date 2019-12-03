bba31c0a8e844c96fc4bc8ad26d5f56337e0f5c4
r-quixote <45530447+r-quixote@users.noreply.github.com>
2019-12-03 15:55:15 +0200

    some minor clean up

--- im2vid_v2.py
+++ im2vid_v2.py
@@ -13,10 +13,12 @@ import traceback
 
 import img_procesing
 import progress_bar
-#from PIL import Image ## for gif
+from PIL import Image ## for gif
 
 def get_orig_path(file_path):
     """
+    used for get_time
+
     will only work if both in same directroy:
     "...\8\side_croped" AND "...\8\side"
     """
@@ -39,68 +41,57 @@ def get_orig_path(file_path):
     return full_or_file_path
 
 def get_time(file_path):
+    """
+    still a bit shaky - ment to get the creation time from the output of lab
+    nikon cams files - not for rasp pi cams
+    """
     if "CROPED" in file_path:
         file_path = get_orig_path(file_path)
         if file_path == "EEROR":
             return("path time not found")
     STAT = os.stat(file_path)
-    return time.strftime('%d-%m %H:%M', time.localtime(STAT[ST_MTIME]))
+    try:
+        creat_time = time.strftime('%d-%m %H:%M', time.localtime(STAT[ST_MTIME]))
+    except:
+        print("there was an error getting the creation time")
+        return("path time not found")
+    return creat_time
 
-
-
-def remove_blue(img):
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    lower_red = np.array([135,0,0])
-    upper_red = np.array([255,255,255])
-    red_mask = cv2.inRange(hsv, lower_red, upper_red)
-
-    lower_yellow = np.array([65,0,0])
-    upper_yellow = np.array([88,255,255])
-    yellow_mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-
-    full_mask = yellow_mask | red_mask
-
-    res = cv2.bitwise_or(img,img, mask= full_mask)
-    return res
-
-
-# =============================================================================
 def create_video(input_folder_path, outvid_path, fps):
     """
     create video from images in input_folder_path
-    FPS NEEDS TO BE FLOAT!
+    fps needs to be a float type!
     """
     imgs_lst = os.listdir(input_folder_path)
 
-    #get size from first img, there might be limit to this, so half it (for now)
+    ## get size from first img
+    ## there might be a limit to what your video player can show
+    ## so we half it (for now)
     image0 = input_folder_path +"\\"+ imgs_lst[0]
     img0 = cv2.imread(image0)
+#    size = ((img0.shape[1]), (img0.shape[0]))
+    size = (int(img0.shape[1]/2), int(img0.shape[0]/2))
 
-    size = ((img0.shape[1]), (img0.shape[0]))
-#    size = (int(img0.shape[1]/2), int(img0.shape[0]/2))
-
-    #set params for the vid_output
+    ## set params for the vid_output
     is_color = True
     fourcc = cv2.VideoWriter_fourcc(*"XVID") ## .avi
-
 #    fourcc = cv2.VideoWriter_fourcc(*'MP4V')  ## .mp4
     vid = cv2.VideoWriter(outvid_path, fourcc, fps, size, is_color)
 
+    ## progress tracking..
     t = time.perf_counter()
-#    frames = [] ## for gif saving
-    try:
-        for i in range(0, int(len(imgs_lst))):
 
+    ## actual loop:
+    try:
+#        frames = []            ## for gif saving
+        for i in range(0, int(len(imgs_lst))):
             image_file = input_folder_path +"\\"+ imgs_lst[i]
             img = cv2.imread(image_file,1)
-            if type(img) != np.ndarray:
+            if type(img) != np.ndarray: ## just trying to catch common errors
                 break
-
-
-    # ========== if anything should be done with img enter code here ==========
+# ========== if anything should be done with img enter code here ==============
 #            img = img_procesing.rotate_img(img,180)
-
+#
 #            dots_img = remove_blue(img)
 #            img = np.concatenate((img, dots_img), axis=1) ## add next to each other
 #            size = (int(img.shape[1]/4), int(img.shape[0]/4))
@@ -109,24 +100,24 @@ def create_video(input_folder_path, outvid_path, fps):
 #            print(size[0]/size[1])
 #            break
 #            img = cv2.resize(img, size)
-
+#
 #            img_procesing.draw_on_img(img, img_procesing.get_time(image_file)) ## add time stamp
-
+#
 #            #save as gif
 #            if i%4 ==0:
 #                img4 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 #                im_pil = Image.fromarray(img4)
 #                frames.append(im_pil)
-    # =========================================================================
+# =============================================================================
 
-            #show img while processing
+            ## show img while processing
             cv2.namedWindow("img", cv2.WINDOW_NORMAL)
             cv2.imshow("img", img)
             cv2.waitKey(1)
 
             vid.write(img)
 
-            ##just for progress tracking:
+            ## just for progress tracking:
             perc = i/len(imgs_lst)
             if perc >0:
                 delt = time.perf_counter() - t
@@ -136,19 +127,19 @@ def create_video(input_folder_path, outvid_path, fps):
                     estimate = str(int(estimate/60))+" min"
                 else: estimate = str(estimate) + " sec"
                 progress_bar.update_progress_bar(perc, " time left ~ " + estimate)
-
         progress_bar.update_progress_bar(1, "\nDone")
+
     except KeyboardInterrupt:
         print("cought: Keyboard Interrupt")
 
-
-#     # for gif saving
+# ==== for gif saving =========================================================
+#
 #    frames[0].save(r'C:\Users\YasmineMnb\Desktop\2_test.gif', format='GIF',
 #                  append_images=frames[1:], save_all=True, duration=100, loop=0)
 #
+# =============================================================================
     vid.release()
     cv2.destroyAllWindows()
-
 
 def create_gif(input_folder_path, output_path):
     from PIL import Image
@@ -169,60 +160,3 @@ def create_gif(input_folder_path, output_path):
 input_folder_path = r"C:\Users\YasmineMnb\Desktop\agueda and cini\100D7500_croped_4"
 outvid_path = r"C:\Users\YasmineMnb\Desktop\agueda and cini\100D7500_croped_4.avi"
 create_video(input_folder_path, outvid_path, 12.0)
-
-
-## gif...
-
-#out_gif = r'C:\Users\YasmineMnb\Desktop\2_test.gif'
-#create_gif(input_folder_path, out_gif)
-#%%
-# ======== Vid2Im =============================================================
-#
-# vidcap = cv2.VideoCapture(r"C:\Users\YasmineMnb\Desktop\for show\lab meeting\s3.mp4")
-# success,image = vidcap.read()
-# cnt = 0
-# while success:
-#     if len(str(cnt)) == 1:
-#         name = "000"+ str(cnt)
-#     elif(len(str(cnt))) == 2:
-#         name = "00" + str(cnt)
-#     elif(len(str(cnt))) == 3:
-#         name = "0" + str(cnt)
-#     elif(len(str(cnt))) == 4:
-#         name = str(cnt)
-#     print(name)
-#     #  print('Read a new frame: ', success)
-#     cnt += 1
-#     success,image = vidcap.read()
-#
-#     cv2.imwrite(r"C:\Users\YasmineMnb\Desktop\test2\frame%s.jpg" % name, image)     # save frame as JPEG file
-#
-# =============================================================================
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# =============================================================================
-#
-# from PIL import Image, ImageDraw
-#
-# images = []
-#
-#
-#
-# im = Image.open("bride.jpg")
-#
-#
-# im.save('out.gif', save_all=True, append_images=[im1, im2, ...])
-#
-# images[0].save(r"C:\Users\YasmineMnb\Desktop\cam_clibration\how_do_drones_work\opencv\pillow_imagedraw.gif",
-#                save_all=True, append_images=images[1:], optimize=False, duration=40, loop=0)
-#
-# =============================================================================
